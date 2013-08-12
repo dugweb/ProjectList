c297aa10d4e5dba31af76b4c70ce6aa0311c0495
Doug Webster <dugweb@gmail.com>
2013-08-12 16:03:01 -0600

    Initial Commit

---
+++ Numbers/calc.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="calcstyle.css">
+
+<body>
+
+
+
+<div id="calculator">
+	
+	<span>Calculator</span>
+	
+	<div id="output"></div>
+
+	<form id="buttons">
+		<!-- Row 1 -->
+	<ul>
+		<li><button id="MC">MC</button></li>
+		<li><button id="Mplus">M+</button></li>
+		<li><button id="Mminus">M-</button></li>
+		<li><button id="Mrecall">MR</button></li>
+	</ul>
+	<!-- Row 2 -->
+	<ul>		
+		<li><button id="Clear">C</button></li>
+		<li><button id="PlusMinus">±</button></li>
+		<li><button id="Divide">÷</button></li>
+		<li><button id="Multiply">×</button></li>
+	</ul>
+
+	<!-- Row 3 -->
+	<ul>		
+		<li><button id="7">7</button></li>
+		<li><button id="8">8</button></li>
+		<li><button id="9">9</button></li>
+		<li><button id="Subtract">-</button></li>
+	</ul>
+
+	<!-- Row 4 -->
+	<ul>		
+		<li><button id="4">4</button></li>
+		<li><button id="5">5</button></li>
+		<li><button id="6">6</button></li>
+		<li><button id="Addition">+</button></li>
+	</ul>
+
+	<!-- Row 5 -->
+	<ul>		
+		<li><button id="1">1</button></li>
+		<li><button id="2">2</button></li>
+		<li><button id="3">3</button></li>
+		<li><button id="Enter">=</button></li>
+	</ul>
+
+	<!-- Row 6 -->
+	<ul>		
+		<li><button id="Zero">0</button></li>
+		<li><button id="Decimal">.</button></li>
+	</ul>
+
+	</form>
+
+</div>
+
+
+
+</body>
+</html>
